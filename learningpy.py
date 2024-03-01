704fa495588e65f94755b8b2a112f2e27bbac962
emmanuelgodfrey2006 <godfrey12345e@gmail.com>
2024-03-01 16:53:16 +0100

    new file

--- learningpy.py
+++ learningpy.py
@@ -5,3 +5,64 @@
 #last_name="Godfrey"
 #full_name=First_name +" "+ last_name
 #print( "Hello"  +" " + full_name )
+
+#faith=favour=Emmanuel=30
+#print(faith)
+#print(favour)
+
+#Thelist= ["apple" , "bababa" , "cherry"]
+#print(Thelist)
+
+#name=input("what is your name?: ")
+#age=(input("How old are you?: "))
+#height=input("how tall are you?: ")
+
+
+#print("Hello "+name)
+#print("You are "+(age)+"years old")
+#print("You are "+(height)+" " "tall")
+
+#name="Bro Code"
+
+#First_name =name[:3]
+
+#print(First_name)
+
+#last_name = name[4:]
+
+#print(last_name)
+
+#website1 ="http://google.com"
+
+#slice = slice(7,-4)
+
+# print(website1[slice])
+
+
+#age = int(input("How old are you: ?"))
+
+#if age >= 18:
+    #print("You are an adult")
+#elif age <= 0:
+    #print("you haven't been born yet")
+#else:
+    #print("u are a a child")    
+
+name = input("what's your name ?: ")
+age = int(input("How old are you ?: "))
+height = float(input("How tall are you ?: "))
+
+
+print("Hello" " " + name)
+
+if age is not int:
+    print("your age can only be a whole number")
+
+if age >= 18 :
+    print("You are an Adult") 
+
+elif age < 18 :
+    print("you are a child")
+
+
+print("you are " + str(height) + "cm " " tall")   
